fdc4242abb980ca7de4ae1741818578bd25f3af4
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-06-23 23:45:54 +0300

    Create Config.cs

---
+++ Entity Framework Core/ADO.NET/Exercises/Villain_Names/Config.cs
@@ -0,0 +1,8 @@
+namespace VillainNames
+{
+    public static class Config
+    {
+        public const string ConnectionString = 
+        @"Server=REPLACE-W-YOUR-PC-NAME-HERE;Database=MinionsDB;Integrated security=True";
+    }
+}
