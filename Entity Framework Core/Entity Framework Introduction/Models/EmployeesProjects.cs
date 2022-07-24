204a5ce59ce6110d79fa65f91449de913aeaddad
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-07-25 00:49:51 +0300

    Create EmployeesProjects.cs

---
+++ Entity Framework Core/Entity Framework Introduction/Models/EmployeesProjects.cs
@@ -0,0 +1,13 @@
+namespace EntityFrameWorkCore.DBFirst.Models
+{
+    public partial class EmployeesProjects
+    {
+        public int EmployeeId { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public virtual Employees Employee { get; set; }
+
+        public virtual Projects Project { get; set; }
+    }
+}
