97622c2c78aa5e2234d5bca9a9bb422663491d77
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-07-28 22:50:29 +0300

    Update StartUp.cs

--- Entity Framework Core/Entity Framework Introduction/StartUp.cs
+++ Entity Framework Core/Entity Framework Introduction/StartUp.cs
@@ -1,58 +1,33 @@
-namespace EntityFrameWorkCore.DBFirst
+namespace SoftUni
 {
+    using SoftUni.Data;
     using System;
     using System.Linq;
     using System.Text;
-    
-    using EntityFrameWorkCore.DBFirst.Data;
 
     public class StartUp
     {
         static void Main(string[] args)
         {
-            var context = new SoftUniContext();
-            
-            // Task 1
-            var output = GetEmployeesFullInformation(context);
-            //
-            
-            // Task 2
-            output = GetEmployeesWithSalaryOver50000(context);
-            //
+            SoftUniContext context = new SoftUniContext();
 
-            // Task 3
-            output = GetEmployeesFromResearchAndDevelopment(context);
-            //
-
-            // Task 4
-
-            //
-
-            // Task 
-
-            //
-
-            // Task 
-
-            //
-            Console.WriteLine(output);
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
         }
 
-        // Task 1
+        // TASK 03. Employees Full Information
         public static string GetEmployeesFullInformation(SoftUniContext context)
         {
-            var sb = new StringBuilder();
+            StringBuilder result = new StringBuilder();
 
             foreach (var emp in context.Employees.OrderBy(e => e.EmployeeId))
             {
-                sb.AppendLine($"{emp.FirstName} {emp.LastName} {emp.MiddleName} {emp.JobTitle} {emp.Salary:f2}");
+                result.AppendLine($"{emp.FirstName} {emp.LastName} {emp.MiddleName} {emp.JobTitle} {emp.Salary:f2}");
             }
 
-            return sb.ToString();
+            return result.ToString();
         }
-        //
 
-        // Task 2
+        // TASK 04. Employees with Salary Over 50 000
         public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
         {
             var sb = new StringBuilder();
@@ -65,13 +40,12 @@ namespace EntityFrameWorkCore.DBFirst
             }
             return sb.ToString();
         }
-        //
 
-        // Task 3
+        // TASK 05. Employees from Research and Development
         public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
         {
             var sb = new StringBuilder();
-           
+
             foreach (var e in context.Employees
                 .Where(d => d.DepartmentId == 6)
                 .OrderBy(s => s.Salary)
@@ -80,22 +54,10 @@ namespace EntityFrameWorkCore.DBFirst
                 sb.AppendLine($"{e.FirstName} {e.LastName} " +
                     $"from Research and Development ${e.Salary:f2}");
             }
-            
+
             return sb.ToString();
         }
-        
-        //
 
-        // Task 4
-
-        //
-
-        // Task 
-
-        //
-
-        // Task 
-
-        //
+        // TASK 06. Adding a New Address and Updating Employee
     }
 }
