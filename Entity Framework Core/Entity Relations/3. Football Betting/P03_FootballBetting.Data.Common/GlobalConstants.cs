aea78918619df622ee6317b2838856050c367aa0
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-08-02 20:18:50 +0300

    Create GlobalConstants.cs

---
+++ Entity Framework Core/Entity Relations/3. Football Betting/P03_FootballBetting.Data.Common/GlobalConstants.cs
@@ -0,0 +1,34 @@
+namespace P03_FootballBetting.Data.Common
+{
+    public static class GlobalConstants
+    {
+        // PLAYER
+        public const int PlayerNameMaxLenght = 60;
+
+        // POSITION
+        public const int PositionNameMaxLenght = 30;
+        
+        // TEAM
+        public const int TeamNameMaxLenght = 50;
+        public const int TeamLogoUrlMaxLenght = 2048;
+        public const int TeamInitialsMaxLenght = 4;
+
+        // COLOR
+        public const int ColorNameMaxLenght = 20;
+
+        // TOWN
+        public const int TownNameMaxLenght = 85;
+
+        // COUNTRY
+        public const int CountryNameMaxLenght = 60;
+
+        // GAME
+        public const int GameResultMaxLenght = 10;
+
+        // USER
+        public const int UserNameMaxLenght = 50;
+        public const int UserPassMaxLenght = 256;
+        public const int EmailMaxLenght = 320;
+        public const int UsernameMaxLenght = 747;
+    }
+}
