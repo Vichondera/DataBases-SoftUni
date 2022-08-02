e4d2caae41b9707a1a69001a3fc8fcbd036d9054
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-08-02 17:15:47 +0300

    Create StartUp.cs

---
+++ Entity Framework Core/Entity Relations/3. Football Betting/P03_FootballBetting/StartUp.cs
@@ -0,0 +1,12 @@
+namespace P03_FootballBetting
+{
+    using System;
+
+   public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
