b3519c75751f24f7c8bc49c52fd09fe8025e7e53
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-08-03 12:56:56 +0300

    Delete Configuration.cs

--- Entity Framework Core/LINQ/MusicHub/MusicHub/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-namespace MusicHub.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=.;Database=MusicHub;Trusted_Connection=True";
-    }
-}
