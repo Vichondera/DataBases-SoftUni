98681da67f2f943b8bf3c0e14f6f5ec7b25938be
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-08-05 23:45:15 +0300

    Update Serializer.cs

--- EFCore-Exams/C# DB Advanced Exam - 04 Dec-2021/01. Model Defition/DataProcessor/Serializer.cs
+++ EFCore-Exams/C# DB Advanced Exam - 04 Dec-2021/01. Model Defition/DataProcessor/Serializer.cs
@@ -1,13 +1,44 @@
-namespace Theatre.DataProcessor
+ namespace Theatre.DataProcessor
 {
     using System;
+    using System.Globalization;
+    using System.Linq;
+
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+
     using Theatre.Data;
 
     public class Serializer
     {
         public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
         {
-            throw new NotImplementedException();
+            var theatres = context.Theatres
+                //.Include(t => t.Tickets)
+                .ToList()
+                .Where(t => t.NumberOfHalls >= numbersOfHalls)
+                .Where(t => t.Tickets.Count >= 20)
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Halls = t.NumberOfHalls,
+                    TotalIncome = Decimal.Parse(t.Tickets
+                    .Where(tc => tc.RowNumber >= 1 && tc.RowNumber <= 5)
+                    .Sum(tc => tc.Price).ToString("F2")),
+                    Tickets = t.Tickets
+                    .Where(tc => tc.RowNumber >= 1 && tc.RowNumber <= 5)
+                    .Select(tc => new
+                    {
+                        Price = Decimal.Parse(tc.Price.ToString("F2")),
+                        RowNumber = tc.RowNumber
+                    })
+                    .OrderByDescending(tk => tk.Price)
+                })
+                .OrderByDescending(t => t.Halls)
+                .ThenBy(t => t.Name)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(theatres,Formatting.Indented);
         }
 
         public static string ExportPlays(TheatreContext context, double rating)
