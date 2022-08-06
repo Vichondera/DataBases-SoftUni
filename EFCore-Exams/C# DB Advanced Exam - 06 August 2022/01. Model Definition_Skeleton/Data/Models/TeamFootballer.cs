e646cd9edbb0ea803ecc7d53aff05fc8b0debe8c
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-08-06 21:56:38 +0300

    Create TeamFootballer.cs

---
+++ EFCore-Exams/C# DB Advanced Exam - 06 August 2022/01. Model Definition_Skeleton/Data/Models/TeamFootballer.cs
@@ -0,0 +1,18 @@
+namespace Footballers.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class TeamFootballer
+    {
+        [Required]
+        [ForeignKey(nameof(Team))]
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Footballer))]
+        public int FootballerId { get; set; }
+        public Footballer Footballer { get; set; }
+    }
+}
